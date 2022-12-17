f0f4f0deaa079ff3d0d0a181e8e3567b56280d44
AlfonsoAnaya <68916711+AlfonsoAnaya@users.noreply.github.com>
2022-12-17 11:18:07 +0100

    Change to solve all exercises

--- readMe.txt
+++ readMe.txt
@@ -1,2 +1,2 @@
-Solutoins to selected problems of Eloquent Javascript.
-I'm keeping an anotated record of the exercises I found most challenging. 
+Solutions to all problems of Eloquent Javascript.
+I'm keeping a record of all the exercises, including those I found most challenging.
---
+++ solutions/2.Program Structure.js
@@ -0,0 +1,14 @@
+// Exercise 1 - Looping a triangle 
+//two ways
+
+for (let string = "#"; string.length < 8; string += "#")
+  console.log(string);
+
+let myString = ""
+for (let counter=0; counter<7; counter++) {
+  	myString += "#"
+	console.log(myString)
+}
+
+//exercise 2 - FizzBuzz
+
