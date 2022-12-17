20e88bc5090721818f92b789a565797a93fc9114
AlfonsoAnaya <68916711+AlfonsoAnaya@users.noreply.github.com>
2022-12-17 12:03:05 +0100

    2.2 exercise added

--- solutions/2.Program Structure.js
+++ solutions/2.Program Structure.js
@@ -10,5 +10,51 @@ for (let counter=0; counter<7; counter++) {
 	console.log(myString)
 }
 
-//exercise 2 - FizzBuzz
+//exercise 2 - FizzBuzz (three different ways)
 
+//shortest
+for (let i=1; i<=100; i++) {
+    if (i%3 === 0 && i%5 ===0) {
+      console.log("fizzbuzz")
+    }
+    else if (i%3 === 0) {
+      console.log("fizz")
+    } else if (i%5 ===0) {
+        console.log("buzz")
+    }
+    else {
+      console.log(i)
+    }
+  }
+  
+//cleares
+  for (let j=1; j<=100; j++) {
+    if (j%3===0 && j%5===0) {
+      console.log("fizzbuzz");
+    }
+    if (j%3===0) {
+      console.log("fizz");
+    }
+    if (j%5===0) {
+      console.log("buzz");
+    }
+    if (!j%3===0 && !j%5===0) {
+      console.log(j);
+    }
+  }
+
+//without && operator
+  for (let k=1; k<=100; k++) {
+    if (k%3===0) {
+      if (k%5===0) {
+        console.log("fizzbuzz");
+      } else {
+          console.log("Fizz"); 
+        }
+      } else if (k%5===0) {
+      console.log("Buzz");
+      } 
+    else {
+    console.log(k);
+      }
+  }
