7aeba18716b0b767b70721c6b8d82a3dc2e87bb0
AlfonsoAnaya <68916711+AlfonsoAnaya@users.noreply.github.com>
2022-12-26 17:41:52 +0100

    added section 3

---
+++ solutions/3. Functions.js
@@ -0,0 +1,13 @@
+//3.1 Minimum
+
+function min(a,b) {
+    if (a<b) {
+      return a
+    } else {
+      return b
+    }
+  }
+  console.log(min(0, 10));
+  // → 0
+  console.log(min(0, -10));
+  // → -10
