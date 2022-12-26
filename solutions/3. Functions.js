42a570dc8be7f859ee1bc4d9602cfc8ad5994f1a
AlfonsoAnaya <68916711+AlfonsoAnaya@users.noreply.github.com>
2022-12-26 18:22:41 +0100

    exercise 3.1 and 3.2

--- solutions/3. Functions.js
+++ solutions/3. Functions.js
@@ -11,3 +11,25 @@ function min(a,b) {
   // → 0
   console.log(min(0, -10));
   // → -10
+
+  //3.2 recursion
+
+  function isEven(num) {
+	if (num == 0) {
+      return true;
+    } else if (num == 1) {
+      return false;
+    } else if (num>0) {
+      num -= 2;
+    } else {
+      num = -num;
+    }
+  	return isEven(num);
+}
+
+console.log(isEven(10));
+// → true
+console.log(isEven(9));
+// → false
+console.log(isEven(-4));
+// → ??
