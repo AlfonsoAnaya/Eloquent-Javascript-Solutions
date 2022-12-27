91da9525c758f1be5dd7d876ffe7c0e31a410fb4
AlfonsoAnaya <68916711+AlfonsoAnaya@users.noreply.github.com>
2022-12-27 11:46:15 +0100

    exercise 3.3

--- solutions/3. Functions.js
+++ solutions/3. Functions.js
@@ -12,6 +12,8 @@ console.log(min(0, 10));
 // → 0
 console.log(min(0, -10));
 // → -10
+
+
 //3.2 recursion
 
 function isEven(num) {
@@ -33,3 +35,33 @@ console.log(isEven(9));
 // → false
 console.log(isEven(-4));
 // → ??
+
+//3.3 Bean counting
+
+/* first version
+function countBs(string) {
+  counter = 0; 
+  for (let i=0; i<string.length; i++) {
+    if (string[i] == "B") counter++;
+  }
+  return counter;
+}
+*/
+
+//second version
+function countBs(string) {
+  return countChar(string, "B");
+}
+
+function countChar(string, char) {
+  counter = 0;
+  for (let i=0; i<string.length; i++) {
+    if (string[i] == char) counter++;
+  }
+  return counter;
+}
+
+console.log(countBs("BYOB at this B&B"));
+// → 2
+console.log(countChar("kakkerlaksk", "k"));
+// → 4
