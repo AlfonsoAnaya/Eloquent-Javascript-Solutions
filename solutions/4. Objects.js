dfef33b73513770e8636b5b8bf7579a2c76d2b5b
AlfonsoAnaya <68916711+AlfonsoAnaya@users.noreply.github.com>
2023-02-02 11:49:38 -0300

    old soution to 4.4

--- solutions/4. Objects.js
+++ solutions/4. Objects.js
@@ -195,3 +195,36 @@ function nth(list, num) {
   return result
 }
 */
+
+//4.4
+//new solution
+
+
+
+/*
+old solution
+function deepEqual(item1, item2) {
+  if (typeof(item1) === "object" && typeof(item2) === "object") {
+    let keys1 = Object.keys(item1)
+  	let keys2 = Object.keys(item2) 
+    if (keys1.length != keys2.length) {
+      return false                  
+    } else {
+      for (let i=0; i<keys1.length; i++) {
+        
+        if (keys1[i] != keys2[i]) {
+          console.log("keys names are different")
+          return false
+        } else {
+          deepEqual(item1[keys1[i]], item2[keys2[i]])
+          if (!result) break
+        }
+      }
+      return result
+    }
+  } else {
+    result = (item1 === item2)
+    return result
+  }
+}
+*/
